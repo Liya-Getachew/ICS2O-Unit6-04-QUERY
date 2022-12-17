2d0571eb8d534eb8c926ecc293ef7087a753cbfd
Liya-Getachew <Liya-Getachew@users.noreply.github.com>
2022-12-17 02:59:07 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -15,21 +15,22 @@ if (navigator.serviceWorker) {
   })
 }
 
-window.onload = function() {
-/**
- * This function calculates the volume of a sphere.
- */
+window.onload = function () {
+  /**
+   * This function calculates the volume of a sphere.
+   */
 
   const params = new URLSearchParams(document.location.search)
 
   // input
-  const radius = params.get('r')
+  const radius = params.get("r")
   console.log(radius)
 
   // process
-  const volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3) 
-  const dimensions = "<ul>\n<li>radius = " + radius 
+  const volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3)
+  const dimensions = "<ul>\n<li>radius = " + radius
 
-  // output 
-  document.getElementById("volume").innerHTML = "<br>Volume = " + volume.toFixed(2) + " cm³"
+  // output
+  document.getElementById("volume").innerHTML =
+    "<br>Volume = " + volume.toFixed(2) + " cm³"
 }
